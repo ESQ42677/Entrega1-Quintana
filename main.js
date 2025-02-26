be5d44d6b03fa3f66065e2fdb573e2dae70bf352
Erika Quintana <erikquintana07@gmail.com>
2025-02-26 20:29:37 -0300

    Entrega1-Quintana

---
+++ main.js
@@ -0,0 +1,29 @@
+
+const nombres1 = "Bienvenidos a Remshop"; 
+console.log (nombres1)
+console.log ('Gracias por tu tiempo!')
+
+ 
+let opcionIngresado =prompt ("Bienvenido/a a nuestra tienda RemShop!, ¿Es tu primera ves en RemShop?")
+let Ingresar =prompt("Ingresa tu nombre")
+let edad= parseInt(prompt("Ingresa tu edad"));
+if (edad === 17 ){
+    console.log ('la condicion es verdadera')
+
+}
+let opcionConfirmar =confirm("Gracias por tus respuestas")
+//ciclos
+if (edad !=17 ){
+    console.log ('la condicion es verdadera');
+ }else{
+    console.log('Como no es mayor,recomendamos limite su entrada al sitio web');
+ }
+
+//array 
+let producto1=[Remeras,Accesorios,Otros]
+
+
+for (let index = 0; i < 3; index++) {
+    console.log (producto1 [i]);
+    
+}
